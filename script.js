dfd0d7375b66e865a2b06195e1ccbbeb79ec8f58
Nikola Kamenovic <nikeyelp@gmail.com>
2025-01-16 15:37:42 +0100

    Basic structure

---
+++ script.js
@@ -0,0 +1,24 @@
+let Vorspeisen = [
+    {
+        "name" : "Griechischer Salat",
+        "price" : 8.90,
+        "description" : "Beschreibung"
+    }
+
+]
+
+let Hauptgerichte = [
+
+]
+
+let Dessert = [
+
+]
+
+let Getränke = [
+
+]
+
+let AlkoholischeGetränke = [
+
+]
