14e8ba2fb5150e0276113d014edd35f7bbe7b6b3
Nikola Kamenovic <nikeyelp@gmail.com>
2025-01-16 16:28:02 +0100

    added dishes to JSON

--- script.js
+++ script.js
@@ -1,24 +1,264 @@
 let Vorspeisen = [
-    {
-        "name" : "Griechischer Salat",
-        "price" : 8.90,
-        "description" : "Beschreibung"
-    }
-
+  {
+    "name": "Tomatensuppe",
+    "price": 4.50,
+    "description": "Frisch zubereitete Suppe aus sonnengereiften Tomaten mit einem Hauch von Basilikum."
+  },
+  {
+    "name": "Bruschetta",
+    "price": 5.00,
+    "description": "Knuspriges Ciabatta-Brot mit einer Mischung aus gehackten Tomaten, Knoblauch und frischem Basilikum."
+  },
+  {
+    "name": "Caesar Salat",
+    "price": 6.50,
+    "description": "Knackiger Römersalat mit Croutons, Parmesan und einem klassischen Caesar-Dressing."
+  },
+  {
+    "name": "Antipasti-Platte",
+    "price": 8.00,
+    "description": "Verschiedene italienische Spezialitäten wie Oliven, gegrilltes Gemüse und luftgetrockneter Schinken."
+  },
+  {
+    "name": "Garnelencocktail",
+    "price": 7.00,
+    "description": "Frische Garnelen in einer leichten Cocktailsauce, serviert auf knackigem Salat."
+  },
+  {
+    "name": "Mozzarella Caprese",
+    "price": 6.00,
+    "description": "Mozzarella und Tomaten mit frischem Basilikum, garniert mit Olivenöl und Balsamico."
+  },
+  {
+    "name": "Kürbissuppe",
+    "price": 4.80,
+    "description": "Cremige Suppe aus Hokkaido-Kürbis mit einer Prise Muskat und Kürbiskernen."
+  },
+  {
+    "name": "Mini-Frühlingsrollen",
+    "price": 5.50,
+    "description": "Knusprige Frühlingsrollen mit einer Füllung aus frischem Gemüse, serviert mit süß-saurer Sauce."
+  },
+  {
+    "name": "Zucchini-Fritters",
+    "price": 6.00,
+    "description": "Knusprig gebratene Zucchini-Puffer, begleitet von einem Joghurt-Dip."
+  },
+  {
+    "name": "Tzatziki mit Pita-Brot",
+    "price": 5.00,
+    "description": "Hausgemachter griechischer Joghurt-Dip mit Knoblauch und Gurken, serviert mit frischem Pita-Brot."
+  }
 ]
 
 let Hauptgerichte = [
-
+  {
+    "name": "Spaghetti Bolognese",
+    "price": 9.50,
+    "description": "Klassische italienische Pasta mit einer herzhaften Fleisch-Tomatensauce und Parmesan."
+  },
+  {
+    "name": "Hähnchen-Curry",
+    "price": 11.00,
+    "description": "Saftiges Hähnchen in einer cremigen Curry-Kokos-Sauce, serviert mit Basmatireis."
+  },
+  {
+    "name": "Rindersteak",
+    "price": 16.50,
+    "description": "Zartes Rindersteak, gegrillt nach Wunsch, mit Kartoffelgratin und grünen Bohnen."
+  },
+  {
+    "name": "Vegetarisches Thai Curry",
+    "price": 10.00,
+    "description": "Würziges Curry mit frischem Gemüse, Kokosmilch und Jasminreis."
+  },
+  {
+    "name": "Fischfilet Provençal",
+    "price": 14.50,
+    "description": "Gebratenes Fischfilet in einer Tomaten-Oliven-Sauce, serviert mit Ratatouille und Reis."
+  },
+  {
+    "name": "Schweineschnitzel Wiener Art",
+    "price": 12.00,
+    "description": "Knusprig paniertes Schweineschnitzel, dazu Pommes frites und ein Beilagensalat."
+  },
+  {
+    "name": "Gemüse-Lasagne",
+    "price": 10.50,
+    "description": "Hausgemachte Lasagne mit Schichten aus Zucchini, Auberginen, Paprika und einer cremigen Bechamelsauce."
+  },
+  {
+    "name": "Ente mit Orangensauce",
+    "price": 15.00,
+    "description": "Knusprige Entenbrust, verfeinert mit einer fruchtigen Orangensauce, dazu Kartoffelklöße und Rotkohl."
+  },
+  {
+    "name": "Burger Deluxe",
+    "price": 12.50,
+    "description": "Saftiges Rindfleisch-Patty im Brioche-Brötchen mit Käse, Speck, Salat und Pommes frites."
+  },
+  {
+    "name": "Veganer Falafel-Teller",
+    "price": 9.00,
+    "description": "Knusprige Falafel-Bällchen, serviert mit Hummus, frischem Salat und Fladenbrot."
+  }
 ]
 
 let Dessert = [
-
+  {
+    "name": "Tiramisu",
+    "price": 5.00,
+    "description": "Klassisches italienisches Dessert mit Mascarpone, Löffelbiskuits und einem Hauch von Kakao."
+  },
+  {
+    "name": "Schokoladenfondant",
+    "price": 6.50,
+    "description": "Warmer Schokoladenkuchen mit einem flüssigen Kern, serviert mit Vanilleeis."
+  },
+  {
+    "name": "Apfelstrudel",
+    "price": 4.50,
+    "description": "Hausgemachter Strudel mit Äpfeln, Zimt und Rosinen, dazu Vanillesauce."
+  },
+  {
+    "name": "Crème Brûlée",
+    "price": 5.50,
+    "description": "Französisches Dessert mit einer knusprigen Karamellschicht und cremigem Vanillepudding."
+  },
+  {
+    "name": "Panna Cotta",
+    "price": 4.80,
+    "description": "Cremiges italienisches Dessert aus Sahne, verfeinert mit Vanille und einem Fruchtspiegel."
+  },
+  {
+    "name": "Mousse au Chocolat",
+    "price": 5.00,
+    "description": "Luftige Schokoladenmousse aus hochwertiger Zartbitterschokolade."
+  },
+  {
+    "name": "Rote Grütze",
+    "price": 4.00,
+    "description": "Fruchtiges Beeren-Dessert, serviert mit einer Portion Vanillesoße."
+  },
+  {
+    "name": "Käsekuchen",
+    "price": 4.50,
+    "description": "Cremiger Käsekuchen auf knusprigem Boden, mit einer leichten Zitronennote."
+  },
+  {
+    "name": "Eisbecher Schoko-Vanille",
+    "price": 4.00,
+    "description": "Kombination aus Schokoladen- und Vanilleeis, garniert mit Sahne und Schokosoße."
+  },
+  {
+    "name": "Baklava",
+    "price": 5.00,
+    "description": "Orientalisches Dessert aus Blätterteig, Nüssen und süßem Honigsirup."
+  }
 ]
 
 let Getränke = [
-
+  {
+    "name": "Mineralwasser",
+    "price": 2.00,
+    "description": "Erfrischendes Mineralwasser, erhältlich still oder mit Kohlensäure."
+  },
+  {
+    "name": "Hausgemachte Limonade",
+    "price": 3.50,
+    "description": "Fruchtige Limonade aus frischen Zitronen, Minze und einem Hauch Rohrzucker."
+  },
+  {
+    "name": "Orangensaft",
+    "price": 3.00,
+    "description": "100% frisch gepresster Orangensaft, ohne Zuckerzusatz."
+  },
+  {
+    "name": "Apfelschorle",
+    "price": 2.80,
+    "description": "Erfrischender Mix aus Apfelsaft und prickelndem Mineralwasser."
+  },
+  {
+    "name": "Eistee Pfirsich",
+    "price": 3.00,
+    "description": "Gekühlter Schwarztee mit einem fruchtigen Pfirsichgeschmack."
+  },
+  {
+    "name": "Mangosaft",
+    "price": 3.50,
+    "description": "Exotischer Mangosaft, pur und vollmundig im Geschmack."
+  },
+  {
+    "name": "Heißer Kakao",
+    "price": 3.20,
+    "description": "Warmer Kakao mit cremiger Sahnehaube."
+  },
+  {
+    "name": "Kaffee",
+    "price": 2.50,
+    "description": "Frisch gebrühter Kaffee, auf Wunsch mit Milch und Zucker."
+  },
+  {
+    "name": "Latte Macchiato",
+    "price": 3.50,
+    "description": "Kaffeespezialität mit geschäumter Milch und einem Schuss Espresso."
+  },
+  {
+    "name": "Ingwer-Zitronentee",
+    "price": 3.00,
+    "description": "Wärmender Tee aus frischem Ingwer und Zitrone, ideal für kalte Tage."
+  }
 ]
 
 let AlkoholischeGetränke = [
-
+  {
+    "name": "Pilsener Bier",
+    "price": 3.00,
+    "description": "Klassisches Pils mit einem frischen, herben Geschmack. (0,5l)"
+  },
+  {
+    "name": "Weißwein Chardonnay",
+    "price": 4.50,
+    "description": "Trockener Weißwein mit einer fruchtigen Note und leichtem Abgang. (0,2l)"
+  },
+  {
+    "name": "Rotwein Merlot",
+    "price": 5.00,
+    "description": "Vollmundiger Rotwein mit Aromen von dunklen Beeren. (0,2l)"
+  },
+  {
+    "name": "Prosecco",
+    "price": 4.50,
+    "description": "Spritziger Schaumwein mit einem Hauch von Zitrusfrüchten. (0,2l)"
+  },
+  {
+    "name": "Whisky Sour",
+    "price": 7.50,
+    "description": "Klassischer Cocktail aus Whisky, Zitronensaft und Zuckersirup, serviert auf Eis."
+  },
+  {
+    "name": "Gin Tonic",
+    "price": 6.50,
+    "description": "Erfrischender Mix aus Gin und Tonic Water, garniert mit einer Scheibe Zitrone."
+  },
+  {
+    "name": "Aperol Spritz",
+    "price": 7.00,
+    "description": "Beliebter Aperitif aus Aperol, Prosecco und einem Spritzer Soda, serviert mit einer Orangenscheibe."
+  },
+  {
+    "name": "Mojito",
+    "price": 8.00,
+    "description": "Karibischer Cocktail aus weißem Rum, frischer Minze, Limette und Soda."
+  },
+  {
+    "name": "Tequila Sunrise",
+    "price": 7.50,
+    "description": "Fruchtiger Cocktail aus Tequila, Orangensaft und Grenadine, mit einem wunderschönen Farbverlauf."
+  },
+  {
+    "name": "Kölsch Bier",
+    "price": 2.80,
+    "description": "Leichtes, obergäriges Bier mit einer milden, süffigen Note. (0,3l)"
+  }
 ]
